f114f2a1a271478e140289ad0f242f96bc0d48d7
Derek Goldstone <derekgoldstone@yahoo.com>
2019-07-10 21:14:11 -0700

    Established server on PORT or 8080 and required routes from savantController.js. Formatted connection.js file to connect to savant_db via sequelize.

---
+++ controllers/savantController.js
@@ -0,0 +1,11 @@
+var express = require("express");
+
+var router = express.Router();
+
+var savant = require("../models/savant.js");
+
+// router.get("/", function(req, res) {
+//     savant.
+// })
+
+module.exports = router;
