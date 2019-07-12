9a8ddb524ddd278e7f1934756fc5a606c21d9735
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-07-12 12:56:08 -0700

    created renderCard function that executes on page load to render cards.

--- public/assets/js/savants.js
+++ public/assets/js/savants.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function () {
 
+    renderCards();
+
     var searchSubmit = $("#search-input").val().trim();
 
     $("#search-submit").on("click", function (event) {
@@ -11,17 +13,16 @@ $(document).ready(function () {
         var search = $("#search-input").val().trim();
 
         $.ajax({
-            type: "POST",
-            url: "/api/classes",   // <-- probably needs new url
+            type: "GET",
+            url: "/api/classes/find/" + search,
             dataType: "json",
-            data: data,   // -- neccesary?
             success: function (data) {
 
                 for (i = 0; i < data.length; i++) {
 
                     if (data.length === 0) {
                         var noResult = $("<h3>")
-                        noResult.html("Unfortunately, no classes exist ")
+                        noResult.html("Unfortunately, no classes exist")
                         $("#card-section").append(noResult)
 
                     } else {
@@ -34,9 +35,6 @@ $(document).ready(function () {
 
                         var cardBody = $("<div>");
                         cardBody.addClass("card-body");
-
-                        var button = $("<button>");
-                        button.addClass("youtube-btn");
 
                         var teacher = data[i].teacher;
                         var title = data[i].title;
@@ -69,3 +67,50 @@ $(document).ready(function () {
 
 
 });
+
+function renderCards() {
+
+
+        $.ajax({
+            type: "GET",
+            url: "/api/classes",
+            dataType: "json",
+            success: function (data) {
+
+                for (i = 0; i < 7; i++) {
+
+                        var item = $("<div>");
+                        item.addClass("item")
+
+                        var card = $("<div>");
+                        card.addClass("card");
+
+                        var cardBody = $("<div>");
+                        cardBody.addClass("card-body");
+
+                        var teacher = data[i].teacher;
+                        var title = data[i].title;
+                        var date = data[i].date;
+                        var description = data[i].desc;
+                        var category = data[i].category;
+                        var youtube = data[i].youtube;
+
+                        cardBody.append("<h5>" + teacher + "</h5>");
+                        cardBody.append("<h6>" + title + "</h6>");
+                        cardBody.append("<p>" + date + "</p>");
+                        cardBody.append("<p>" + description + "</p>");
+                        cardBody.append("<p>" + category + "</p>");
+
+                        card.append(youtube);
+                        card.append(cardBody);
+
+                        item.append(card);
+
+                        $("#card-section").append(item);
+
+
+                }
+            }
+        });
+
+}
