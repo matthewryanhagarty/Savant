2ecf183f15eb8b50363c2e3cca57a0895b7860d6
Phillip Chang <phillip-chang@live.com>
2019-07-11 22:00:24 -0700

    Create function to display all users and all classes

--- controllers/savantController.js
+++ controllers/savantController.js
@@ -2,32 +2,26 @@ var path = require("path");
 var db = require("../models");
 
 module.exports = function(app) {
+
+    // Route to Landing Page
     app.get("/", function(req,res){
         res.sendFile(path.join(__dirname, "../public.index.html"));
     });
 
-    app.post("/api/teacher", function(req,res) {
+    // Route to Create a Class Page
+    app.get("/api/teacher", function(req,res) {
         res.sendFile(path.join(__dirname, "../public/teacher.hmtl"));
     });
-    
+
+    // Display All Classes
     app.get("/api/classes", function(req,res) {
         db.Classes.findAll({})
             .then(function(dbClasses){
                 res.json(dbClasses);
             });
     });
-    
-    app.get("/api/users/:name", function(req,res) {
-        db.User.findAll({
-            where: {
-                name: req.params.name
-            }
-        })
-            .then(function(dbUser){
-                res.json(dbUser);
-            });
-    });
-    
+
+    // Search Specific Class
     app.get("/api/classes/:title", function(req,res) {
         db.User.findAll({
             where: {
@@ -39,6 +33,30 @@ module.exports = function(app) {
             });
         });
 
+    // Display All Users
+    app.get("/api/users", function(req,res) {
+        db.User.findAll({})
+        .then(function(dbUser){
+            res.json(dbUser);
+        });
+    });
+
+    // Search Specific User
+    app.get("/api/users/:name", function(req,res) {
+        db.User.findAll({
+            where: {
+                name: req.params.name
+            }
+        })
+            .then(function(dbUser){
+                res.json(dbUser);
+            });
+    });
+
+    app.post("/api/users/register", function(req,res) {
+    
+    });
+    
 
 };
 
