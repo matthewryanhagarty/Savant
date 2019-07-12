22b87867d32fc2f0ddb6abad91cd1643dda792a9
Phillip Chang <phillip-chang@live.com>
2019-07-11 21:55:36 -0700

    Create function to search specific class and user and to create event page

--- controllers/savantController.js
+++ controllers/savantController.js
@@ -6,12 +6,38 @@ module.exports = function(app) {
         res.sendFile(path.join(__dirname, "../public.index.html"));
     });
 
+    app.post("/api/teacher", function(req,res) {
+        res.sendFile(path.join(__dirname, "../public/teacher.hmtl"));
+    });
+    
     app.get("/api/classes", function(req,res) {
         db.Classes.findAll({})
             .then(function(dbClasses){
                 res.json(dbClasses);
             });
     });
+    
+    app.get("/api/users/:name", function(req,res) {
+        db.User.findAll({
+            where: {
+                name: req.params.name
+            }
+        })
+            .then(function(dbUser){
+                res.json(dbUser);
+            });
+    });
+    
+    app.get("/api/classes/:title", function(req,res) {
+        db.User.findAll({
+            where: {
+                title: req.params.title
+            }
+        })
+            .then(function(dbClass){
+                res.json(dbClass);
+            });
+        });
 
 
 };
