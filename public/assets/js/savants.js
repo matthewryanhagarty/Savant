cb6359f832559af8fe34301202587a405bbc589c
Matthew Ryan Hagarty <matthewryanhagarty@gmail.com>
2019-07-11 21:39:39 -0700

    AJAX to the grab data from server. working on code to display the cards.

--- public/assets/js/savants.js
+++ public/assets/js/savants.js
@@ -1,28 +1,67 @@
 // search bar on-click --> display results //
 
+$(document).ready(function() {
+
+    var cardContainer = $(".wrapper");
+    var searchSubmit = $("#search-input").val().trim();
+
 $("#search-submit").on("click", function(event) {
     
     event.preventDefault();
     
-    var searchSubmit = $("#search-inpit").val().trim();
+    var search = $("#search-input").val().trim();
 
     $.ajax({
         type: "POST",
-        url: "/api/search",   // <-- probably needs new url
+        url: "/api/classes",   // <-- probably needs new url
         dataType: "json",
-        data: data,
+        data: data,   // -- neccesary?
         success: function(data) {
-          console.log(data);   //What the server replies with
+
+            for (i = 0; i < data.length; i++) {
+
+                if (response.data.length === 0) {
+                    var noResult = $("<h1>")
+                    noResult.css("font-size", "30px")
+                    noResult.html("No results")
+                    $("#card-section").append(noResult)
+                    
+                } else {
+
+                var item = $("<div>")
+
+
+            }
+
+
         }
       });
 
 
 
-    //   function searchSubmit(search) {
-    //     $.post("/api/posts/", Post, function() {
-
-            
-
-    //     });
     
+});
+
+
 })
+
+
+
+
+
+// function getCards(search) {
+//     var searchString = search || "";
+//     if (searchString) {
+//       searchString = "/classes/" + searchString;
+//     }
+//     $.get("/api/posts" + searchString, function(data) {
+//       console.log("Posts", data);
+//       posts = data;
+//       if (!posts || !posts.length) {
+//         displayEmpty();
+//       }
+//       else {
+//         initializeRows();
+//       }
+//     });
+//   }
