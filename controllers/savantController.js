7c6f306f3b67d3f443620a51b475f5d15a22a041
Phillip Chang <phillip-chang@live.com>
2019-07-11 22:07:06 -0700

    Remove commented out lines of unused code

--- controllers/savantController.js
+++ controllers/savantController.js
@@ -70,17 +70,3 @@ module.exports = function(app) {
     
 
 };
-
-
-
-// var express = require("express");
-
-// var router = express.Router();
-
-// var Savant = require("../models/savant.js");
-
-// // router.post("/", function(req, res) {
-// //     savant.
-// // })
-
-// module.exports = router;
