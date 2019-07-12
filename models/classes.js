3ed23080beffb8f8ca81465b8217621447e09355
Esar Behlum <esarnb@gmail.com>
2019-07-12 13:15:16 -0700

    fixing routes for express with models

--- models/classes.js
+++ models/classes.js
@@ -1,60 +1,48 @@
-var Sequelize = require("sequelize");
-var env = process.env.NODE_ENV || "development";
-var config = require(__dirname + "/../config/config.json")[env];
-
-if (config.use_env_variable) {
-    var sequelize = new Sequelize(process.env[config.use_env_variable]);
-  } else {
-    var sequelize = new Sequelize(config.database, config.username, config.password, config);
-  }
-
-
-var Classes = sequelize.define("Classes", {
+module.exports = function(sequelize, DataTypes) {
+    var Classes = sequelize.define("Classes", {
         title: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             allowNull: false,
             validate: {
                 len: [2, 50]
             }
         },
         desc: {
-            type:  Sequelize.STRING,
+            type:  DataTypes.STRING,
             allowNull: false,
             validate: {
                 len: [2, 100]
             }
         },
         date: {
-            type: Sequelize.DATEONLY,
+            type: DataTypes.DATEONLY,
             allowNull: false,
             validate: {
                 isDate: true
             }
         },
         liveLink: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             allowNull: false,
             validate: {
                 isUrl: true
             }
         },
         teacher: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             allowNull: false,
         },
         categ: {
-            type: Sequelize.STRING,
+            type: DataTypes.STRING,
             allowNull: false,
             validate: {
                 len: [2, 10]
             }
         },
         uuid: {
-            type: Sequelize.UUID,
+            type: DataTypes.UUID,
             allowNull: false
         } 
     });
-
-Classes.sync();
-
-module.exports = Classes;
+    return Classes;
+};
