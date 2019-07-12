aa5775e15917fd5a24ca744c497e6a6cd56e49e2
Phillip Chang <phillip-chang@live.com>
2019-07-11 21:36:00 -0700

    Add exports function for root file route

--- controllers/savantController.js
+++ controllers/savantController.js
@@ -1,11 +1,23 @@
-var express = require("express");
+var path = require("path");
+var db = require("../models");
 
-var router = express.Router();
+module.exports = function(app) {
+    app.get("/", function(req,res){
+        res.sendFile(path.join(__dirname, "../public.index.html"));
+    });
 
-var Savant = require("../models/savant.js");
+};
 
-// router.post("/", function(req, res) {
-//     savant.
-// })
 
-module.exports = router;
+
+// var express = require("express");
+
+// var router = express.Router();
+
+// var Savant = require("../models/savant.js");
+
+// // router.post("/", function(req, res) {
+// //     savant.
+// // })
+
+// module.exports = router;
